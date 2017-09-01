2b442b04c4fba7c209025b64a2202271e49d2669
sam <nullvoidnil@mail.com>
2017-09-01 18:31:16 -0400

    Moved the storage functions from trys to futures

--- src/main/scala/server.scala
+++ src/main/scala/server.scala
@@ -1,17 +1,15 @@
 package server
 
-
 import com.twitter.{util => Util, io => IO , finagle => Finagle} 
 import org.iq80.leveldb.Options
-import store.{util => store_util}, store._
 
-
+import storage.{util => store_util}, storage._
 import scodec.bits._
 import scodec._, codecs.{list => listEncode, utf8}
 
 import State._ 
 import Finagle.http.{Request, Response, Status, Method}, Util.{Future, Try}
-
+import java.util.concurrent.{ForkJoinPool}, Util.{FuturePool, ExecutorServiceFuturePool}
 
 
 import Finagle.http.path._, Finagle.Service, Finagle.Failure
@@ -19,7 +17,7 @@ import Finagle.http.path._, Finagle.Service, Finagle.Failure
 object BucketApi {
 
   import Ops._ 
-  def create(buckets: BucketMap, bpath: String): Future[Response] = Future.const { BucketMap.mkBucket(buckets, bpath) }.map {b =>
+  def create(buckets: BucketMap, p: FuturePool,  bpath: String): Future[Response] = BucketMap.mkBucket(buckets, p ,  bpath).map {b =>
     val r = Response(Status.Ok)
     r.setContentString(s"Added ${bpath} to store.")
     r
@@ -77,20 +75,20 @@ object kv_api {
   }
 
   def get(s: BucketMap, path: String, key: Array[Byte]) = {
-    val tx = (b: Bucket) =>  store_util.optAsTry(  KV.get(b, key) )
+    val tx = (b: Bucket) => KV.get(b, key) 
 
     performOP(s, path, tx).map { d =>
       val rep = Response(Status.Ok) 
-      val buf = store_util.asBuf(d)
+      val buf = store_util.Buffers.asBuf(d)
       rep.content(buf); rep 
     }
 
   }
 
 
-  def performOP[T](bmap: BucketMap, path: String, op: Bucket => Try[T] ): Future[T] = {
+  def performOP[T](bmap: BucketMap, path: String, op: Bucket => Future[T] ): Future[T] = {
     val t = store_util.optAsTry( BucketMap.getBucket(bmap, path) )
-    Future.const(t) flatMap { b => Future.const( op(b) ) }
+    Future.const(t) flatMap { b => op(b)  }
   }
 
 
@@ -122,6 +120,15 @@ object Handlers {
 
   import IO.Buf.ByteArray.Shared
 
+  case class Resources(bm: BucketMap, pool: FuturePool = defaultPool)
+
+  def defaultPool = {
+    val fjp = new ForkJoinPool(6) 
+    new ExecutorServiceFuturePool(fjp) 
+  }
+
+
+
   def KV(bm: BucketMap) = Service.mk[Request, Response] { req => (req.method, Path(req.path)  ) match {
 
     //Delete 
@@ -142,24 +149,24 @@ object Handlers {
 
    
 
-  def Buckets(bm: BucketMap) = Service.mk[Request, Response] { req => (req.method, Path(req.path) ) match {
+  def Buckets(r: Resources) = Service.mk[Request, Response] { req => (req.method, Path(req.path) ) match {
 
     case Method.Post -> root / "api" / "v1" /  "buckets" =>
       val p = req.contentString
-      BucketApi.create(bm, p)
+      BucketApi.create(r.bm, r.pool, p)
 
-    case Method.Get -> root / "api" / "v1" / "buckets" => BucketApi.list(bm)
+    case Method.Get -> root / "api" / "v1" / "buckets" => BucketApi.list(r.bm)
 
-    case Method.Delete -> root / "api" / "v1" / "buckets" / p => BucketApi.delete(bm, p)
+    case Method.Delete -> root / "api" / "v1" / "buckets" / p => BucketApi.delete(r.bm, p)
 
   } } 
 
   import Finagle.http.HttpMuxer
 
-  def Server(bm: BucketMap) = {
-
+  def Server(r: Resources) = {
+    val bm = r.bm 
     val kv = KV(bm)
-    val buckets = Buckets(bm)
+    val buckets = Buckets(r)
 
     val muxer = new HttpMuxer()
       .withHandler("/api/v1/kv", kv)
--- src/test/scala/store.scala
+++ src/test/scala/store.scala
@@ -1,69 +1,63 @@
-package store 
+
+package storage
+
 import org.scalatest.{FunSuite}
 import java.io.File
 import com.twitter.util._
-import org.fusesource.leveldbjni.JniDBFactory._
+
+
 
 object Helpers {
-  def assertOption[T](o: Option[T]) = o match {case Some(v) => true; case None => false} 
-}
+  def assertOption[T](o: Option[T]) = o match {case Some(v) => true; case None => false}
 
-class BlockStoreTest extends FunSuite {
-  import BlockStore._
-  import java.nio.file.{Files, Paths}
-  
-  val opts = util.configure()
-
-  test ("opt gen") { assert( opts.createIfMissing() ) }
-   
-
-  val p = "/home/sam/fucknigga.db"
-  //val d = Try { factory.destroy(new File(p), opts ) } 
-  val b = Bucket.make(p, opts)
-
-  test ("bucket creation") {
-    assert { Files.exists( Paths.get(p) ) }
-    assert (b.isReturn )
-  }
-
-  test ("CRUD Operations") {
-    val bckt = b.get 
-    val t = Bucket.put(bckt, "n".getBytes, "coon".getBytes)
-    assert(t.isReturn)
-
-    val g = Bucket.get(bckt, "n".getBytes)
-    val gb = g match {case Some(res) => true ; case None => false}
-
-    assert(gb)
-    assert( ( new String(g.get) ).trim()  == "coon" ) 
-
-    val del = Bucket.delete(bckt, "n".getBytes)
-    assert(del.isReturn)
-  }
-
-
-  test ("bucket destruction") {
-    val del = Bucket.destroy(b.get)
-    assert(del.isReturn) 
-  }
-
-}
-
-
-class  BucketMapTest extends FunSuite {
-  import KV.BucketMap
-
-  val tbl = BucketMap.make
-
-  test ("CRUD") {
-    val bm = BucketMap.mkBucket(tbl, "slut")
-    assert {bm.isReturn }
-
-    val bck = BucketMap.getBucket(tbl, "slut")
-    assert { Helpers.assertOption(bck) }
-
-    val del = BucketMap.rmBucket(tbl, "slut")
-    assert(del.isReturn)
+  def assertFuture[T](f: Future[T]) = f.poll match {
+    case Some(x) => x.isReturn
+    case _ => false 
   } 
 
+  def assertTry[T](t: Try[T]) = t.isReturn
+
+
+}
+
+
+
+
+class OpsSuite extends FunSuite {
+  import Ops._ , Helpers._
+
+  val pool = FuturePool.unboundedPool
+  val b = Await.result( Bucket.make("pals", pool, util.configure()  ) )
+
+  val (k, v) = ("buddy".getBytes, "guy".getBytes)
+
+
+  test ( "Put and Get " ) {  
+    Await.result( KV.put(b, k, v) )
+    val res = KV.get(b, k)
+    assertFuture( res )
+    assert( new String( Await.result(res) ) == new String( v ) ) 
+  }
+
+
+  test ("Delete and Get ")  {
+    Await.result(KV.delete(b, k) )
+    assert { assertFuture( KV.get(b, k) ) != true }
+  }
+
+  test("put and list") {
+    val t = Await.result( KV.put(b, k, v) )
+    val l = KV.list(b)
+    assert { assertFuture(l) }
+    assert { Await.result(l) == List(new String(k) )  } 
+  }
+
+
+  test ("Destroy") {
+    val op = Bucket.destroy(b)
+    Await.result( op )
+    assert { assertFuture(op) }
+
+    
+  }
 }
